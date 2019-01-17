7b725b59f0370470a00937f5b027b3e44fa7716f
Laimonas Katkus <laimonas@katkus.eu>
2019-01-17 20:53:02 +0200

    Added splash screen

--- android/app/src/main/java/com/reactnativetypescripttemplate/MainActivity.java
+++ android/app/src/main/java/com/reactnativetypescripttemplate/MainActivity.java
@@ -1,6 +1,26 @@
 package com.reactnativetypescripttemplate;
 
+import android.os.Bundle;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
+
+import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(this.createSplashLayout());
+    }
+
+    public LinearLayout createSplashLayout() {
+        LinearLayout splash = new LinearLayout(this);
+        Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(), R.drawable.launch_screen);
+        splash.setBackground(launch_screen_bitmap);
+
+        return splash;
+    }
 }
